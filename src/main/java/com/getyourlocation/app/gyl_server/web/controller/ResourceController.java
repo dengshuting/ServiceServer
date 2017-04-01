740f5d4f1c6e5ea5e5dd7dd8fc7a2ffb806ffe77
Steven Liu <stevennl@foxmail.com>
2017-04-01 23:48:05 +0800

    Update sum

--- src/main/java/com/getyourlocation/app/gyl_server/web/controller/ResourceController.java
+++ src/main/java/com/getyourlocation/app/gyl_server/web/controller/ResourceController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,17 +21,17 @@ public class ResourceController {
     private static final Logger LOG = LoggerFactory.getLogger(ResourceController.class);
 
     @RequestMapping(path = "/sum", method = RequestMethod.POST)
-    public LinkedHashMap<String, Object> testCompute(HttpServletRequest request, HttpServletResponse response) {
-        Enumeration enu=request.getParameterNames();
-        int sum = 0;
-        while(enu.hasMoreElements()){
-            String paraName=(String)enu.nextElement();
-            sum = sum + Integer.parseInt(request.getParameter(paraName));
-        }
+    public LinkedHashMap<String, Object> testCompute(
+        @RequestParam(value = "x", defaultValue = "0") int x,
+        @RequestParam(value = "y", defaultValue = "0") int y,
+        HttpServletRequest request, HttpServletResponse response) {
+
         LOG.info(request.getMethod() + " " + request.getRequestURI());
+
         LinkedHashMap<String, Object> result = new LinkedHashMap<String, Object>();
-        result.put("answer", sum);
+        result.put("answer", x + y);
         return result;
+
     }
 
 }
