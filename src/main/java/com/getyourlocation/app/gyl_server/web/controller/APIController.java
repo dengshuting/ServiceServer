304b65b320610b592dea4822a365a990245f6c9f
xuanqu <xuan_qu@foxmail.com>
2017-05-07 22:14:17 +0800

    triangle localize

--- src/main/java/com/getyourlocation/app/gyl_server/web/controller/APIController.java
+++ src/main/java/com/getyourlocation/app/gyl_server/web/controller/APIController.java
@@ -3,12 +3,10 @@ package com.getyourlocation.app.gyl_server.web.controller;
 import com.getyourlocation.app.gyl_server.util.LogUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -82,12 +80,43 @@ public class APIController {
 
     @RequestMapping(path = "/localization", method = RequestMethod.GET)
     public LinkedHashMap<String, Object> localization(
+            //** case 1: alpha=45, beta=45, (x1,y1)=(-1,0), (x2,y2)=(0,-1), (x3,y3)=(1,0), expected:P(0,1),actual:P(0,0.4142)
+            // case 2: alpha=60, beta=60, (x1,y1)=(-1.732,0), (x2,y2)=(0,-1), (x3,y3)=(1.732,0), expected:P(0,1),actual:P(~0,0.99)/(0.211,1.095)(original)
+            // case 3: alpha=45, beta=45, (x1,y1)=(-1,-1), (x2,y2)=(1,-1), (x3,y3)=(1,1), expected:P(-1,1), actual:P(-1,1)
+            // case 4: alpha=90, beta=53, (x1,y1)=(-4,0), (x2,y2)=(0,0), (x3,y3)=(4,3), expected:P(0,3), actual:P(0.010,2.999)
+            // case 4: alpha=25.43, beta=76.9, (x1,y1)=(-1.8,3.8), (x2,y2)=(1,0.5), (x3,y3)=(2.5,0.8), expected:P(1.6,4), actual:P(1.599,4.00)
+        @RequestParam(value = "alpha", defaultValue = "25.43") double alpha,
+        @RequestParam(value = "beta", defaultValue = "76.9") double beta,
+        @RequestParam(value = "x1", defaultValue = "-1.8") double x1,
+        @RequestParam(value = "y1", defaultValue = "3.8") double y1,
+        @RequestParam(value = "x2", defaultValue = "1") double x2,
+        @RequestParam(value = "y2", defaultValue = "0.5") double y2,
+        @RequestParam(value = "x3", defaultValue = "2.5") double x3,
+        @RequestParam(value = "y3", defaultValue = "0.8") double y3,
         HttpServletRequest request, HttpServletResponse response) {
         LogUtil.logReq(LOG, request);
         LinkedHashMap<String, Object> result = new LinkedHashMap<String, Object>();
-        result.put("x", 136.35);
-        result.put("y", 230.55);
+
         // TODO 定位
+        double a = Math.sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
+        double b = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        //double theta = Math.toDegrees(Math.acos((x2*x2 + y2*y2 - x1*x2 - y1*y2 - x2*x3 - y2*y3 + x1*x3 + y1*y3) / (a*b)));  // paper
+        double theta = Math.toDegrees(Math.acos(((x3-x2)*(x1-x2)+(y3-y2)*(x3-x2))/(a*b)));
+
+        double cotA = 1 / Math.tan(Math.toRadians(alpha));
+        double sinB = Math.sin(Math.toRadians(beta));
+        double sinBT = Math.sin(Math.toRadians(beta + theta));
+        double cosBT = Math.cos(Math.toRadians(beta + theta));
+
+        double x0 = (a * b * (sinBT * cotA + cosBT) * (a * sinB * cotA + b * cosBT)) / (Math.pow(b * sinBT - a * sinB, 2) + Math.pow(b * cosBT + a * sinB * cotA, 2));
+        double y0 = (a * b * (sinBT * cotA + cosBT) * (b * sinBT - a * sinB)) / (Math.pow(b * sinBT - a * sinB, 2) + Math.pow(b * cosBT + a * sinB * cotA, 2));
+
+        float x = (float)(x0 * ((x3-x2)/a) - y0 * ((y3-y2)/a) + x2);
+        float y = (float)(x0 * ((y3-y2)/a) + y0 * ((x3-x2)/a) + y2);
+
+        result.put("x", x);
+        result.put("y", y);
+
         return result;
     }
 }
